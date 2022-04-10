7047b257b803bc03a1507fca70663740aea61a1e
swanine <haohao1500@foxmail.com>
2022-04-10 10:10:15 +0800

    🔧 chore: template init: sass, tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  render() {
+    return (
+      <>
+        <HelloWorld></HelloWorld>
+      </>
+    )
+  }
+})
