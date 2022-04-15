16240c052640aa4b6413d382928ea06094c060a9
chenzhou <944695474@qq.com>
2022-04-15 15:59:55 +0800

    chore: add jest

---
+++ src/components/Test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="test-container page-container">
+    <div class="page-title">Unit Test Page</div>
+    <p>count is: {{ count }}</p>
+    <button @click="increment">increment</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  setup() {
+    const count = ref<number>(0)
+    const increment = () => {
+      count.value += 1
+    }
+    return { count, increment }
+  }
+})
+</script>
