fefcbf9fe89bd98f285408cb03387fbe5425ea69
chenzhou <944695474@qq.com>
2022-04-15 15:23:50 +0800

    chore: change config server alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src') // 设置 `@` 指向 `src` 目录
+    }
+  },
+  base: './', // 设置打包路径
   server: {
     port: 8001,
-    open: true
+    open: true,
+    cors: true
+
+    // 设置代理，根据我们项目实际情况配置
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://xxx.xxx.xxx.xxx:8000',
+    //     changeOrigin: true,
+    //     secure: false,
+    //     rewrite: (path) => path.replace('/api/', '/')
+    //   }
+    // }
   }
 })
